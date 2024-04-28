a6af2845f545ae3913710d7c821fd8df83fac165
Blaga Filip-Andrei <167856935+UnTipOarecare@users.noreply.github.com>
2024-04-28 22:39:32 +0300

    example1 dar uman.c

--- example1.c
+++ example1.c
@@ -12,84 +12,84 @@ typedef struct Node {
 typedef struct Graph {
     int vertices;
     int *visited;
-    Node **adjacency_list;
+    Node **adjacencyList;
 } Graph;
 
-Node *create_node(int vertex) {
-    Node *new_node = static_cast<Node *>(malloc(sizeof(Node)));
-    new_node->vertex = vertex;
-    new_node->next = NULL;
+Node *createNode(int vertex) {
+    Node *newNode = static_cast<Node *>(malloc(sizeof(Node)));
+    newNode->vertex = vertex;
+    newNode->next = NULL;
 
-    return new_node;
+    return newNode;
 }
 
-void add_edge(Graph *graph, int source, int destination) {
-    Node *new_node = create_node(destination);
-    new_node->next = graph->adjacency_list[source];
-    graph->adjacency_list[source] = new_node;
+void addEdge(Graph *graph, int source, int destination) {
+    Node *newNode = createNode(destination);
+    newNode->next = graph->adjacencyList[source];
+    graph->adjacencyList[source] = newNode;
 }
 
-Graph *create_graph(int vertices) {
+Graph *createGraph(int vertices) {
     Graph *graph = static_cast <Graph*> (malloc(sizeof(Graph)));
 
     graph->vertices = vertices;
-    graph->adjacency_list = static_cast<Node **>(malloc(sizeof(Node *) * vertices));
+    graph->adjacencyList = static_cast<Node **>(malloc(sizeof(Node *) * vertices));
     graph->visited = static_cast<int *>(malloc(sizeof(int) * vertices));
 
     for (int i = 0; i < vertices; i++) {
-        graph->adjacency_list[i] = NULL;
+        graph->adjacencyList[i] = NULL;
         graph->visited[i] = 0;
     }
 
     return graph;
 }
 
-void depth_first_search(Graph *graph, int vertex_number, int destination, int *path_exists) {
-    Node *adj_list = graph->adjacency_list[vertex_number];
-    Node *aux = adj_list;
-    graph->visited[vertex_number] = 1;
+void depthFirstSearch(Graph *graph, int vertexNumber, int destination, int *pathExists) {
+    Node *adjList = graph->adjacencyList[vertexNumber];
+    Node *aux = adjList;
+    graph->visited[vertexNumber] = 1;
 
-    if (vertex_number == destination) {
-        *path_exists = 1;
+    if (vertexNumber == destination) {
+        *pathExists = 1;
         return;
     }
 
     while (aux != NULL) {
-        int connected_vertex = aux->vertex;
-        if (graph->visited[connected_vertex] == 0) {
-            depth_first_search(graph, connected_vertex, destination, path_exists);
+        int connectedVertex = aux->vertex;
+        if (graph->visited[connectedVertex] == 0) {
+            depthFirstSearch(graph, connectedVertex, destination, pathExists);
         }
         aux = aux->next;
     }
 }
 
 int main() {
-    int num_vertices, num_edges, source, destination;
+    int numVertices, numEdges, source, destination;
 
     printf("Cate noduri are reteaua?\n");
-    scanf("%d", &num_vertices);
+    scanf("%d", &numVertices);
 
     printf("Cate muchii are reteaua?\n");
-    scanf("%d", &num_edges);
+    scanf("%d", &numEdges);
 
-    Graph *graph = create_graph(num_vertices);
+    Graph *graph = createGraph(numVertices);
 
-    printf("Adauga %d muchii (de la 1 la %d) (exemplu: \"%d 1\"):\n", num_edges, num_vertices, num_vertices);
+    printf("Adauga %d muchii (de la 1 la %d) (exemplu: \"%d 1\"):\n", numEdges, numVertices, numVertices);
 
-    for (int i = 0; i < num_edges; i++) {
+    for (int i = 0; i < numEdges; i++) {
         int source, destination;
         scanf("%d %d", &source, &destination);
-        add_edge(graph, source - 1, destination - 1); // decrementăm pentru a indexa de la 0
+        addEdge(graph, source - 1, destination - 1); // decrementăm pentru a indexa de la 0
     }
 
     printf("Introduceti sursa si destinatia pentru a verifica existenta unui drum direct:\n");
     scanf("%d %d", &source, &destination);
 
-    int path_exists = 0;
+    int pathExists = 0;
 
-    depth_first_search(graph, source - 1, destination - 1, &path_exists); // decrementăm pentru a indexa de la 0
+    depthFirstSearch(graph, source - 1, destination - 1, &pathExists); // decrementăm pentru a indexa de la 0
 
-    if (path_exists) {
+    if (pathExists) {
         printf("Exista un drum direct intre nodurile %d si %d.\n", source, destination);
     } else {
         printf("Nu exista un drum direct intre nodurile %d si %d.\n", source, destination);
